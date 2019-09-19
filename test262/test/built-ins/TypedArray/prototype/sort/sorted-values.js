ab3744f29a60442aa8aa020fb3f67a50ec7a55fc
seven332 <seven332@163.com>
2019-09-19 18:10:32 +0800

    2019-09-18

--- test262/test/built-ins/TypedArray/prototype/sort/sorted-values.js
+++ test262/test/built-ins/TypedArray/prototype/sort/sorted-values.js
@@ -28,6 +28,16 @@ testWithTypedArrayConstructors(function(TA) {
 });
 
 testWithTypedArrayConstructors(function(TA) {
+  var sample = new TA([1, 0, -0, 2]).sort();
+  assert(compareArray(sample, [-0, 0, 1, 2]), "0s");
+}, floatArrayConstructors);
+
+testWithTypedArrayConstructors(function(TA) {
+  var sample = new TA([1, 0, -0, 2]).sort();
+  assert(compareArray(sample, [0, 0, 1, 2]), "0s");
+}, intArrayConstructors);
+
+testWithTypedArrayConstructors(function(TA) {
   var sample = new TA([-4, 3, 4, -3, 2, -2, 1, 0]).sort();
   assert(compareArray(sample, [-4, -3, -2, 0, 1, 2, 3, 4]), "negative values");
 }, [Float64Array, Float32Array, Int8Array, Int16Array, Int32Array]);
@@ -40,9 +50,6 @@ testWithTypedArrayConstructors(function(TA) {
 
   sample = new TA([0.5, 0, 1.5, -0.5, -1, -1.5, 1]).sort();
   assert(compareArray(sample, [-1.5, -1, -0.5, 0, 0.5, 1, 1.5]), "non integers + negatives");
-
-  sample = new TA([1, 0, -0, 2]).sort();
-  assert(compareArray(sample, [-0, 0, 1, 2]), "0 and -0");
 
   sample = new TA([3, 4, Infinity, -Infinity, 1, 2]).sort();
   assert(compareArray(sample, [-Infinity, 1, 2, 3, 4, Infinity]), "infinities");
